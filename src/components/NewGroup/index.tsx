07e5f5b04f3be97d19f38af73af2b66543ea50d8
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-09-24 20:52:11 -0300

    chore: CSS in JS - Estilizando o placeholder da interface Input

--- src/components/NewGroup/index.tsx
+++ src/components/NewGroup/index.tsx
@@ -1,7 +1,8 @@
 import { Container, Content, Icon } from "./styles";
 
-import { Header } from "@components/Header";
+import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
 export function NewGroup() {
@@ -17,7 +18,9 @@ export function NewGroup() {
           subtitle="crie uma turma para adicionar pessoas"
         />
 
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
