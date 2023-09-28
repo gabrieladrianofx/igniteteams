759d3352909c0f80c4b297c6a5778e6d9427fa26
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-09-27 21:26:44 -0300

    chore: Navegação - Estrutura de Navegação

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 
 import theme from "./src/theme";
 
-import { Players } from "@screens/Players";
+import { Routes } from "@routes/index";
 import { Loading } from "@components/Loading";
 
 export default function App() {
@@ -25,7 +25,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
