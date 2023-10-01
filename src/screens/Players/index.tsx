e46979df5ad5c285a4b1adb50ad8bc028acbafa7
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-10-01 20:56:29 -0300

    chore: Local Storage - Carregando Players pelo Grupo e Time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
-import { useRoute } from "@react-navigation/native";
+
 import { Alert, FlatList } from "react-native";
 
+import { useRoute } from "@react-navigation/native";
+
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
+import { AppError } from "@utils/AppError";
+
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
 
 import { Input } from "@components/Input";
 import { Header } from "@components/Header";
@@ -12,9 +20,7 @@ import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { PlayerCard } from "@components/PlayerCard";
-import { AppError } from "@utils/AppError";
-import { playerAddByGroup } from "@storage/player/playerAddByGroup";
-import { playersGetByGroup } from "@storage/player/playersGetByGroup";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
   group: string;
@@ -23,7 +29,7 @@ type RouteParams = {
 export function Players() {
   const [newPlayerName, setNewPlayerName] = useState("");
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
@@ -52,6 +58,19 @@ export function Players() {
         Alert.alert("Nova pessoa", "Não foi possível adicionar.");
         console.log(error);
       }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team);
+      setPlayers(playersByTeam);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Pessoas",
+        "Não foi possível carregar as pessoas do time selecionado."
+      );
     }
   }
 
