8368f2057fa0990679b207291aa647c5f04617c8
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-10-01 18:48:22 -0300

    chore: Local Storage - Identificando o foco na tela

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import { FlatList } from "react-native";
 
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -20,6 +22,21 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,6 +4,8 @@ import { useNavigation } from "@react-navigation/native";
 
 import { Container, Content, Icon } from "./styles";
 
+import { groupCreate } from "@storage/group/groupCreate";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -13,8 +15,13 @@ export function NewGroup() {
   const [group, setGroup] = useState("");
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate("players", { group });
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
